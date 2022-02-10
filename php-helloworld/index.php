c49ae1d3b8029f7bb04637ec8e3d5dd6cc303a85
Student User <student@workstation.lab.example.com>
2022-02-09 21:10:09 -0500

    Changed index content

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming\n";
-print "Este es un nuevo cambio\n";
+print "Este es un nuevo cambio  con los muchachos!\n";
 ?>
